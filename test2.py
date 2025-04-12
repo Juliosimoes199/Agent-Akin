53b2bc7400744749dbb425dbbc4c2b736e530ec2
Julio Simões <Juliocesarsimoes100@gmail.com>
2025-04-12 12:30:27 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -9,8 +9,9 @@ import sys
 sys.stdout.reconfigure(encoding='utf-8')
 import logging
 logging.basicConfig(level=logging.DEBUG)  # Define o nível de log para DEBUG
+from flask import Flask, request, jsonify
 
-
+app = Flask(__name__)
 
 load_dotenv()
 
@@ -25,33 +26,49 @@ class Noticias(BaseModel):
 
 controller = Controller(output_model=Noticias)
 
-async def main():
-    task = """
-Log in using the email: 'jpaulo@gmail.com' and password 'jpa2024' and bring me the updated URL
-"""
-    llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp", api_key="AIzaSyArTog-quWD9Tqf-CkkFAq_-UOZfK1FTtA")
-    initial_actions = [
-        {'open_tab': {'url': 'https://akin-lis-app-web.vercel.app/akin/schedule/new'}}
-    ]
+@app.route('/')
+def hello_world():
+    return 'Olá do Flask!'
 
-    agent = Agent(
-        task=task,
-        controller=controller,
-        initial_actions=initial_actions,
-        llm=llm,
-        message_context="This is a platform that contains a user email field and password to log in"
-       
 
-    )
-
-    history = await agent.run()  # Certifique-se de que isto esteja dentro de uma função assíncrona
-    result = history.final_result()
-    print(history)
-    for step in history.steps:
-        print(f"Step {step['number']}: {step['action']} - Status: {step['status']}")
-
+@app.route('/test')
+def test():
+    async def main():
+        task = """
+    Log in using the email: 'jpaulo@gmail.com' and password 'jpa2024' and bring me the updated URL
+    """
+        llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp", api_key="AIzaSyArTog-quWD9Tqf-CkkFAq_-UOZfK1FTtA")
+        initial_actions = [
+            {'open_tab': {'url': 'https://akin-lis-app-web.vercel.app/akin/schedule/new'}}
+        ]
     
+        agent = Agent(
+            task=task,
+            controller=controller,
+            initial_actions=initial_actions,
+            llm=llm,
+            message_context="This is a platform that contains a user email field and password to log in"
+           
+    
+        )
+    
+        history = await agent.run()  # Certifique-se de que isto esteja dentro de uma função assíncrona
+        result = history.final_result()
+        print(history)
+        for step in history.steps:
+            print(f"Step {step['number']}: {step['action']} - Status: {step['status']}")
+    
+        
+    
+    # Esta linha é usada para iniciar a função assíncrona 'main'
+    asyncio.run(main())
 
-# Esta linha é usada para iniciar a função assíncrona 'main'
-asyncio.run(main())
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
+
+
 
